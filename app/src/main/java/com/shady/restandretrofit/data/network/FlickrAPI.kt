d83e4c6aee47572ca2f53dda9c146fe4f455cc30
Shady Selim <shady_andela@medable.com>
2021-11-03 15:27:29 +0300

    Increasing key visibility security

--- app/src/main/java/com/shady/restandretrofit/data/network/FlickrAPI.kt
+++ app/src/main/java/com/shady/restandretrofit/data/network/FlickrAPI.kt
@@ -1,10 +1,11 @@
 package com.shady.restandretrofit.data.network
 
+import com.shady.restandretrofit.BuildConfig
 import com.shady.restandretrofit.data.models.FlickrData
 import retrofit2.Call
 import retrofit2.http.GET
 
 interface FlickrAPI {
-    @GET("?method=flickr.interestingness.getList&api_key=1429a3752b5d2658fecb7bd8d46d71bc&format=json&nojsoncallback=1&extras=url_s")
+    @GET("?method=flickr.interestingness.getList&api_key=${BuildConfig.flickrAPIKey}&format=json&nojsoncallback=1&extras=url_s")
     suspend fun fetchPhotos(): FlickrData
 }
