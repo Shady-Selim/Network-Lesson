515f2d071f55bac1fa79ce8ef84da10a35e64c02
Shady Selim <shady_andela@medable.com>
2021-11-04 15:11:57 +0300

    Added SharedPreference

--- app/src/main/java/com/shady/restandretrofit/ui/MainActivity.kt
+++ app/src/main/java/com/shady/restandretrofit/ui/MainActivity.kt
@@ -1,11 +1,14 @@
 package com.shady.restandretrofit.ui
 
+import android.content.Context
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.SearchView
+import android.widget.Toast
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.RecyclerView
 import androidx.recyclerview.widget.StaggeredGridLayoutManager
@@ -16,19 +19,26 @@ class MainActivity : AppCompatActivity() {
     private val vm by lazy {
         ViewModelProvider(this).get(MainVM::class.java)
     }
+    private lateinit var sharedPreferences: SharedPreferences
+    private val SHARED_KEY = "lastSearch"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         flickrRV = findViewById(R.id.rvFlickr)
         flickrRV.layoutManager = StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL)  //GridLayoutManager(this, 2)
-
+        sharedPreferences = this.getSharedPreferences("flickrSearchSharePreference", Context.MODE_PRIVATE)
         loadFlickrImages()
     }
 
     private fun loadFlickrImages(query: String? = null) {
         vm.fetchInterestingList(query).observe(this, {
-            flickrRV.adapter = FlickrAdapter(it.photos.photo)
+            if(query.isNullOrEmpty()){
+                flickrRV.adapter = FlickrAdapter(it.photos.photo)
+            } else {
+                flickrRV.swapAdapter(FlickrAdapter(it.photos.photo), false)
+            }
             Log.d("Flickr Main Response", it.photos.toString())
         })
     }
@@ -42,6 +52,12 @@ class MainActivity : AppCompatActivity() {
             setOnQueryTextListener(object : SearchView.OnQueryTextListener{
                 override fun onQueryTextSubmit(query: String?): Boolean {
                     Log.d(TAG, "QueryTextSubmit: $query")
+                    val sharePref = sharedPreferences.getString(SHARED_KEY, "This was your 1st Search!! Yay!!")
+                    Toast.makeText(context, sharePref, Toast.LENGTH_LONG).show()
+                    sharedPreferences
+                        .edit()
+                        .putString(SHARED_KEY, "Your last search was: $query")
+                        .apply()
                     loadFlickrImages(query?.trim())
                     return true
                 }
